8202204203d14b3056bce49d8a3de2bf19f4f004
Ahmad Awadalla <167709857+ahmadawadalla@users.noreply.github.com>
2025-03-12 18:27:45 -0700

    updated version

--- GalleonClicker/index.js
+++ GalleonClicker/index.js
@@ -714,7 +714,7 @@ function numberString(number){
             word = numString[i]
         else {
             number /= (10 ** (3 * (1 + i)))
-            return (Math.floor(number * 10) / 10).toLocaleString() + ' ' + word
+            return (Math.floor(number * 100) / 100).toLocaleString() + ' ' + word
         }
     }
 }
